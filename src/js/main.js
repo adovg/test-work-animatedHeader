87db641f080d662df8fddb97ba28c7236d414989
Oleksii <kroatoan@i.ua>
2021-05-01 02:19:40 +0300

    animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,67 +1,86 @@
-// let myScroll = window.scrollY;
-// console.log(myScroll);
-
-// var height = document.body.scrollHeight;
-// console.log(`scrollheight is ${height}` ); когда равен этому значению, снимаем фикс прыгаем к якорю
-
-// let height = document.documentElement.clientHeight;
-// console.log(height); или это вариант получения высоты
-
-//let myCircleHeight = document.querySelector('.hero__content').clientHeight;  первый рабочий вариант 
-
-
-
-
-// let myCircle = document.querySelector('.hero__content');
-// let myCircleHeight = window.getComputedStyle(myCircle, ).height;
-//  console.log(myCircleHeight);
-
-//  window.onscroll = magic;
-
-//  function magic(){
-//    // 500 расстояние при котором событие произойдет 
-//    if (window.pageYOffset > 0) {
-// 	 section.style.height = '500px'
-//    } else {
-// 	 section.style.height = '200px'
-//    }
-//  }
-
-
-// window.addEventListener('scroll', function() {
-// 	console.log(pageYOffset + 'px');
-// 	let myCircleHeight = document.querySelector('.hero__content').clientHeight;
-// 	document.querySelector('.hero__content').clientHeight = myCircleHeight + pageYOffset
-
-//   });
-
-//   document.onmousewheel=document.onwheel=function(){ 
-// 	return false;
-// };
-
-
-// var element = document.querySelector('.hero__content');
-// var hp   = document.body.scrollHeight,
-//     hw   = window.innerHeight,
-//     smax = hp - hw,
-//     smin = 0,
-//     hmin = 450,
-//     hmax = 450,
-//     ds   = smax - smin,
-//     dh   = hmax - hmin;
-
-// window.addEventListener('scroll', function () {
-//     var s = window.pageYOffset; // current scrollTop
-// 	console.log(s);
-//     var h = hmax - (dh * s) / ds;
-
-//     element.style.height = h + 'px';
+// Shorthand for $( document ).ready()
+// $(function() {
+//     console.log("JQUERY IS READY!");
 // });
 
-// document.querySelector(".container").addEventListener("wheel", myFunction);
 
-// function myFunction() {
-// 	console.log()
-//   this.style.fontSize = "35px";
-// }
+var height = $(".hero__content").height();
+var width = $(".hero__content").width();
+console.log(height);
 
+const pageWidth = document.documentElement.scrollWidth;
+const pageHeight = document.documentElement.scrollHeight;
+var windowInnerHeight = document.documentElement.clientHeight
+
+
+$(window).bind('mousewheel DOMMouseScroll', function(event){
+    if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+    console.log('scroll up');
+
+        // scroll up
+        $(".hero__content").width(width -= 60);
+        $(".hero__content").height(height -= 60);
+        console.log(height);
+        if (height <= 450 ) {
+            $(".header").show();
+            $(".menu__container").show();
+           // $(".navigation").hide();
+           height = 450;
+           width = 450;
+        }
+
+    }
+    else {
+    
+        // scroll down
+        $(".hero__content").width(width += 60);
+        $(".hero__content").height(height += 60);
+        console.log(windowInnerHeight);
+        console.log(height);
+        if (height >= windowInnerHeight) {
+            $(".header").hide();
+            $(".menu__container").hide();
+        }
+
+    }
+});
+
+
+
+
+let animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll);
+  function animOnScroll() {
+    for (let i = 0; i < animItems.length; i++) {
+      const animItem = animItems[i];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      console.log(animItemOffset);
+      const animStart = 3 //coef start animation
+      
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      
+      if (animItemHeight > window.innerHeight) {animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                                               }
+      
+      if( (pageYOffset > animItemOffset - animItemPoint)  && pageYOffset < (animItemOffset + animItemHeight) ) {
+        animItem.classList.add('-active');
+      } else if (!animItem.classList.contains('_anim-no-hide')) {
+        animItem.classList.remove('-active');
+      }
+    }
+  }
+  
+    /////get offset func 
+
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+          scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+          scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
+  //animOnScroll();
+  setTimeout( () => {animOnScroll()}, 300);
+}
