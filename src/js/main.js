cb0213d5a3624292f0e58e2320f899bfb64ef0ff
Oleksii <kroatoan@i.ua>
2021-05-01 16:47:07 +0300

    adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,43 @@
-// Shorthand for $( document ).ready()
-// $(function() {
-//     console.log("JQUERY IS READY!");
-// });
-
 
 var height = $(".hero__content").height();
 var width = $(".hero__content").width();
-console.log(height);
 
 const pageWidth = document.documentElement.scrollWidth;
 const pageHeight = document.documentElement.scrollHeight;
-var windowInnerHeight = document.documentElement.clientHeight
+var windowInnerHeight = document.documentElement.clientHeight;
+var windowInnerWidth = document.documentElement.clientWidth;
 
 
-$(window).bind('mousewheel DOMMouseScroll', function(event){
-    if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-    console.log('scroll up');
+$(window).bind('mousewheel DOMMouseScroll', function (event) {
+  if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
 
-        // scroll up
-        $(".hero__content").width(width -= 60);
-        $(".hero__content").height(height -= 60);
-        console.log(height);
-        if (height <= 450 ) {
-            $(".header").show();
-            $(".menu__container").show();
-           // $(".navigation").hide();
-           height = 450;
-           width = 450;
-        }
-
+    // scroll up
+    $(".hero__content").width(width -= 60);
+    $(".hero__content").height(height -= 60);
+    if (height <= 450) {
+      $(".header").show();
+      $(".menu__container").show();
+      $(".hero__content").width(width = 450);
+      $(".hero__content").height(height = 450);
+      $(".navigation").hide();
+      $(".about").hide();
+      // width = 450;
     }
-    else {
-    
-        // scroll down
-        $(".hero__content").width(width += 60);
-        $(".hero__content").height(height += 60);
-        console.log(windowInnerHeight);
-        console.log(height);
-        if (height >= windowInnerHeight) {
-            $(".header").hide();
-            $(".menu__container").hide();
-        }
 
+  }
+  else {
+
+    // scroll down
+    $(".hero__content").width(width += 60);
+    $(".hero__content").height(height += 60);
+    if (height >= windowInnerHeight) {
+      $(".header").hide();
+      $(".menu__container").hide();
+      $(".navigation").show();
+      $(".about").show();
     }
+
+  }
 });
 
 
@@ -57,30 +52,41 @@ if (animItems.length > 0) {
       const animItem = animItems[i];
       const animItemHeight = animItem.offsetHeight;
       const animItemOffset = offset(animItem).top;
-      console.log(animItemOffset);
       const animStart = 3 //coef start animation
-      
+
       let animItemPoint = window.innerHeight - animItemHeight / animStart;
-      
-      if (animItemHeight > window.innerHeight) {animItemPoint = window.innerHeight - window.innerHeight / animStart;
-                                               }
-      
-      if( (pageYOffset > animItemOffset - animItemPoint)  && pageYOffset < (animItemOffset + animItemHeight) ) {
+
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
         animItem.classList.add('-active');
       } else if (!animItem.classList.contains('_anim-no-hide')) {
         animItem.classList.remove('-active');
       }
     }
-  }
-  
-    /////get offset func 
+  };
+
+  /////get offset func 
 
   function offset(el) {
     const rect = el.getBoundingClientRect(),
-          scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-          scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
   }
   //animOnScroll();
-  setTimeout( () => {animOnScroll()}, 300);
-}
+  setTimeout(() => { animOnScroll() }, 300);
+};
+
+
+$(".hero__arrow").on("click", function () {
+
+  $([document.documentElement, document.body]).animate({
+    scrollTop: $("#scroll-anchor").offset().top
+  }, 1000);
+
+});
+
+
